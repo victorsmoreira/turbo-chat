4bad67ff19da1495ed0c264a9b4cf116f446db7b
Victor Moreira <victorsmoreira@outlook.com>
2022-10-09 19:11:11 -0300

    room and message models

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_13_203858) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_09_220341) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "room_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.text "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
@@ -22,4 +36,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_13_203858) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "messages", "rooms"
 end
